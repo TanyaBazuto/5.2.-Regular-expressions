21fb69db8e0a37fad278ea646cf83dcf302a0587
Tatiana Bazuto <tbazuto@gmail.com>
2024-03-14 21:47:31 +0300

    Add main

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import re
+from pprint import pprint
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# читаем адресную книгу в формате CSV в список contacts_list
+with open('phonebook_raw.csv', encoding='utf-8') as f:
+  rows = csv.reader(f, delimiter=',')
+  contacts_list = list(rows)
+# pprint(contacts_list)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# TODO 1: выполните пункты 1-3 ДЗ
+# функция объединения дублирующихся записей о контакте
+def union_contact(contacts: list):
+  for contact in contacts:
+    first_name = contact[0]
+    last_name = contact[1]
+    for new_contact in contacts:
+      new_first_name = new_contact[0]
+      new_last_name = new_contact[1]
+      if first_name == new_first_name and last_name == new_last_name:
+        if contact[2] == '': contact[2] = new_contact[2]
+        if contact[3] == '': contact[3] = new_contact[3]
+        if contact[4] == '': contact[4] = new_contact[4]
+        if contact[5] == '': contact[5] = new_contact[5]
+        if contact[6] == '': contact[6] = new_contact[6]
+
+  result_list = list()
+  for i in contacts:
+    if i not in result_list:
+      result_list.append(i)
+
+  return result_list
+
+# функция приведения формата телефона к единому формату
+def create_contact(contact_list: list):
+  pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+  substitution = r'+7(\2)\3-\4-\5 \6\7'
+  new_list = list()
+  for item in contact_list:
+      full_name = ' '.join(item[:3]).split(' ')
+      result = [full_name[0], full_name[1], full_name[2], item[3], item[4],
+                re.sub(pattern, substitution, item[5]),
+                item[6]]
+      new_list.append(result)
+  return union_contact(new_list)
+
+pprint(create_contact(contacts_list))
+replace_list = create_contact(contacts_list)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# TODO 2: сохраните получившиеся данные в другой файл
+# код для записи файла в формате CSV
+with open('phonebook.csv', 'w', encoding='utf-8') as f:
+  datawriter = csv.writer(f, delimiter=',')
+  datawriter.writerows(replace_list)
